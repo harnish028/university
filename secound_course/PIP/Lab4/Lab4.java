29687cadc04b603baf68a6d1a42b3f137ff33eb5
Dmitry Parshikov <dmitry.parshikov28@gmail.com>
2016-11-23 23:19:15 +0300

    Update Lab4.java

--- secound_course/PIP/Lab4/Lab4.java
+++ secound_course/PIP/Lab4/Lab4.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.*;
@@ -12,19 +14,20 @@ import java.util.List;
 import static javax.swing.SwingUtilities.invokeLater;
 
 public class Lab4 extends JFrame {
-    Graphics g;
-    public static final int FRAME_WIDTH = 500;
-    public static final int FRAME_HEIGHT = 580;
+    private static final int FRAME_HEIGHT = 580;
+    private static final int FRAME_WIDTH = 500;
+
+    int R = 10;
+    Form startForm = new Form(R);
+    private List<Dot> puntos = new ArrayList<>();
+    private List<Dot> targets = new ArrayList<>();
     Dot mainDot = new Dot(0, 0);
-    List<Dot> dots = new ArrayList<>();
+
     private JPanel dataPanel;
     private JPanel xListPanel;
     private JPanel yRaddioPanel;
     private JPanel rSliderPanel;
     private DrawPanel drawPanel;
-    private JButton drawDotButton;
-    private JPanel forDrawPanel;
-
 
     private JTextArea nowDot;
     private JList<String> xList;
@@ -38,35 +41,79 @@ public class Lab4 extends JFrame {
     private JLabel rLabel;
     private JSlider rSlider;
 
+    private JPanel  buttonPanel;
+    private JButton clearButton;
+    private JButton drawButton;
+
+    List<Thread> threadList = new ArrayList<>();
+    List<DotAnimation> dotAnimations = new ArrayList<>();
+
     public Lab4() {
         frameSettings();
         addListX();
         addRadioButtonY();
         addSliderR();
         addTextArea();
-        addButton();
         addDrawPanel();
+        addButtonPanel();
 
         dataPanel = new JPanel(new GridLayout(0, 4));
         dataPanel.add(xListPanel);
         dataPanel.add(yRaddioPanel);
         dataPanel.add(nowDot);
-        dataPanel.add(drawDotButton);
+        dataPanel.add(buttonPanel);
 
-
-        forDrawPanel = new JPanel();
-        forDrawPanel.add(drawPanel);
         add(rSliderPanel, BorderLayout.WEST);
         add(dataPanel, BorderLayout.SOUTH);
         add(drawPanel, BorderLayout.CENTER);
-        // add(forDrawPanel, BorderLayout.CENTER);
+    }
 
+    public void addButtonPanel() {
+        buttonPanel = new JPanel(new GridLayout(2,0));
+        addDrawButton();
+        addClearButton();
+    }
+
+    public void addClearButton() {
+        clearButton = new JButton("CLEAR");
+
+        ActionListener actionListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                repaint();
+                puntos.clear();
+            }
+        };
+
+        buttonPanel.add(clearButton);
+        clearButton.addActionListener(actionListener);
+    }
+
+    public void addDrawButton() {
+        drawButton = new JButton("DRAW");
+        ActionListener actionListener = e -> {
+           // drawPanel.paint(drawPanel.getGraphics());
+            puntos.add(mainDot);
+            nowDot.setText(mainDot.toString());
+            if(startForm.isContain(mainDot)) {
+                targets.add(mainDot);
+//                DotAnimation nextDot = new DotAnimation(drawPanel.getGraphics(), drawPanel, mainDot);
+//                Thread animationThread = new Thread(nextDot);
+//                animationThread.start();
+                drawPanel.startAnimation(drawPanel.getGraphics());
+            }
+            showDots(drawPanel.getGraphics(), puntos, startForm, R);
+            showDots(drawPanel.getGraphics(), targets, startForm, R);
+        };
+
+        buttonPanel.add(drawButton);
+        drawButton.addActionListener(actionListener);
     }
 
     public void addListX() {
         xListPanel = new JPanel(new GridLayout(0, 1));
         int[] x = new int[]{0, 4, -2, 10, 5};
-        xList = new JList<String>((new String[]{"x = 0", "x = 4", "x = -2", "x = 10", "x = 5"}));
+        xList = new JList<>((new String[]{"x = 0", "x = 4", "x = -2", "x = 10", "x = 5"}));
 
         ListSelectionListener listSelectionListener = new ListSelectionListener() {
             @Override
@@ -81,9 +128,29 @@ public class Lab4 extends JFrame {
 
     public void addSliderR() {
         rSliderPanel = new JPanel(new GridLayout(0, 2));
-        rSlider = new JSlider(JSlider.VERTICAL, 0, 30, 5);
+        rSlider = new JSlider(JSlider.VERTICAL, 0, 50, 5);
         rLabel = new JLabel("R");
 
+        ChangeListener changeListener = new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                drawPanel.paint(drawPanel.getGraphics());
+                R = ((JSlider)e.getSource()).getValue();
+                startForm = new Form(R);
+                for(Dot dots:puntos) {
+                    if(startForm.isContain(dots)) {
+//                        DotAnimation nextDot = new DotAnimation(drawPanel.getGraphics(), drawPanel, dots);
+//                        Thread animationThread = new Thread(nextDot);
+//                        animationThread.start();
+                        drawPanel.startAnimation(drawPanel.getGraphics());
+                        break;
+                    }
+                }
+                showDots(drawPanel.getGraphics(), puntos, startForm, R);
+            }
+        };
+
+        rSlider.addChangeListener(changeListener);
         rSliderPanel.add(rSlider);
         rSliderPanel.add(rLabel);
     }
@@ -119,6 +186,8 @@ public class Lab4 extends JFrame {
             }
         };
 
+        y0.setSelected(true);
+
         y0.addActionListener(actionListener);
         y1.addActionListener(actionListener);
         y2.addActionListener(actionListener);
@@ -143,28 +212,12 @@ public class Lab4 extends JFrame {
 
     public void addTextArea() {
         nowDot = new JTextArea();
-    }
-
-    public JTextArea getTextAr() {
-        return nowDot;
+        nowDot.setText(mainDot.toString());
     }
 
     public void addDrawPanel() {
         drawPanel = new DrawPanel();
-    }
-
-    public void addButton() {
-        drawDotButton = new JButton("DRAW");
-        ActionListener actionListener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                g = ((DrawPanel) e.getSource()).getGraphics();
-                g.setColor(Color.BLACK);
-                dots.add(mainDot);
-            }
-        };
-
-        drawDotButton.addActionListener(actionListener);
+        drawPanel.addMouseListener(new DrawMouseListener());
     }
 
     public static void main(String[] args) {
@@ -186,179 +239,34 @@ public class Lab4 extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
-    private class DrawPanel extends JPanel {
+    public double toDecartX(double x, double R) { return (x - DrawPanel.CENTER_X) * R / DrawPanel.START_RADIUS;}
 
-        public  final Color COLOR_BLACK = new Color(0, 0, 0);
-        public  final Color LIGHT_GREEN = new Color(129, 192, 187);
-        public  final Color COLOR_BROWN = new Color(150, 75, 0);
-        public  Color areaColor = COLOR_BLACK;
+    public double toDecartY(double y, double R) {
+        return (-y + DrawPanel.CENTER_Y) * R / DrawPanel.START_RADIUS;
+    }
 
-
-        public static final int HEIGHT = 9;
-        public static final int WEIGHT = 9;
-        public  int CENTER_X;
-        public  int CENTER_Y;
-        public static final int START_RADIUS = 100;
-        public static final int POINT_RADIUS = 5;
-        public static final int MARGIN = 9;
-        public JTextArea a = new JTextArea();
-        private List<Dot> puntos = new ArrayList<Dot>();
-        DotAnimation dotAnimation;
-        Form start = new Form(10);
-
-        public DrawPanel() {
-            setBackground(LIGHT_GREEN);
-            setSize(462, 462);
-            addMouseListener(new DrawMouseListener());
+    public void showDots(Graphics g, List<Dot> dots, Form form, int R) {
+        for(Dot dot : dots) {
+            if(form.isContain(dot)) {
+                g.setColor(Color.GREEN);
+                g.fillOval((int) dot.toPixelsX(R) - 2, (int) dot.toPixelsY(R) - 2, DrawPanel.POINT_RADIUS, DrawPanel.POINT_RADIUS);
+            } else {
+                g.setColor(Color.RED);
+                g.fillOval((int) dot.toPixelsX(R) - 2, (int) dot.toPixelsY(R) - 2, DrawPanel.POINT_RADIUS, DrawPanel.POINT_RADIUS);
+            }
         }
+    }
 
-        public void paintComponent(Graphics g) {
-            super.paintComponent(g);
-
-            int w = this.getWidth();
-            int h = this.getHeight();
-
-            CENTER_X = this.getWidth() >> 1;
-            CENTER_Y = (this.getHeight() + 1) >> 1;
-
-            drawCoordinate(g, h, w);
-            drawHatching(g, h, w);
-            drawArea(g, Color.BLUE);
-
-        }
-
-        public void drawArea(Graphics g, Color color) {
-            g.setColor(color);
-            drawRectangle(g, color);
-            drawTriangle(g, color);
-            drawArc(g, color);
-        }
-
-        private void drawArrow(Graphics g, int h, int w) {
-            g.drawLine(w / 2, 0, w / 2 - 5, 10);
-            g.drawLine(w / 2, 0, w / 2 + 5, 10);
-            g.drawLine(w, h / 2, w - 10, h / 2 + 5);
-            g.drawLine(w, h / 2, w - 10, h / 2 - 5);
-        }
-
-        private void drawCoordinate(Graphics g, int h, int w) {
-            g.setColor(Color.BLACK);
-            g.drawLine(w / 2, 0, w / 2, h);
-            g.drawString("Y", w / 2 + MARGIN, MARGIN);
-
-            g.drawLine(0, h / 2, w, h / 2);
-            g.drawString("X", w - MARGIN / 2, h / 2 + MARGIN);
-
-            g.drawString("R", w / 2 + 5, h / 2 - START_RADIUS);
-            g.drawString("R / 2", w / 2 + 5, h / 2 - START_RADIUS / 2);
-            g.drawString("0", w / 2 - 7, h / 2 + 10);
-
-            g.drawString("R", w / 2 + START_RADIUS + 2, h / 2 + 15);
-            g.drawString("-R", w / 2 - START_RADIUS - 5, h / 2 + 15);
-            g.drawString("-R / 2", w / 2 - START_RADIUS / 2 - 18, h / 2 + 15);
-
-            g.drawString("-R", w / 2 - 15, h / 2 + START_RADIUS);
-            g.drawString("-R / 2", w / 2 - 30, h / 2 + START_RADIUS / 2);
-
-            drawArrow(g, h, w);
-            System.out.println(puntos);
-        }
-
-        private void drawRectangle(Graphics g, Color color) {
-            g.fillRect(CENTER_X, CENTER_Y, START_RADIUS, START_RADIUS);
-        }
-
-        private void drawArc(Graphics g, Color color) {
-            g.fillArc(CENTER_X - START_RADIUS / 2, CENTER_Y - START_RADIUS / 2, START_RADIUS, START_RADIUS, 0, 90);
-        }
-
-        private void drawTriangle(Graphics g, Color color) {
-            int[] xCoordinate = {CENTER_X, CENTER_X - START_RADIUS, CENTER_X};
-            int[] yCoordinate = {CENTER_Y - START_RADIUS, CENTER_Y, CENTER_Y};
-            g.fillPolygon(xCoordinate, yCoordinate, 3);
-        }
-
-        public void drawHatching(Graphics g, int h, int w) {
-            g.setColor(Color.WHITE);
-            g.drawLine(w / 2 - 3, h / 2 - START_RADIUS, w / 2 + 3, h / 2 - START_RADIUS);
-            g.drawLine(w / 2 - 3, h / 2 - START_RADIUS / 2, w / 2 + 3, h / 2 - START_RADIUS / 2);
-            g.drawLine(w / 2 - START_RADIUS, h / 2 - 3, w / 2 - START_RADIUS, h / 2 + 3);
-            g.drawLine(w / 2 - START_RADIUS / 2, h / 2 - 3, w / 2 - START_RADIUS / 2, h / 2 + 3);
-            g.drawLine(w / 2 + START_RADIUS / 2, h / 2 - 3, w / 2 + START_RADIUS / 2, h / 2 + 3);
-            g.drawLine(w / 2 + START_RADIUS, h / 2 - 3, w / 2 + START_RADIUS, h / 2 + 3);
-            g.drawLine(w / 2 - 3, h / 2 + START_RADIUS, w / 2 + 3, h / 2 + START_RADIUS);
-            g.drawLine(w / 2 - 3, h / 2 + START_RADIUS / 2, w / 2 + 3, h / 2 + START_RADIUS / 2);
-        }
-
-        public double toDecartX(double x, double R) {
-            return (x - CENTER_X) * R / DrawPanel.START_RADIUS;
-        }
-
-        public double toDecartY(double y, double R) {
-            return (-y + CENTER_Y) * R / DrawPanel.START_RADIUS;
-        }
-
-        public void startAnimation(Graphics g, int radius, List<Dot> puntos) {
-            new Thread(new Runnable() {
-                public void run() {
-                    try {
-                        int r0 = 1;
-                        int g0 = 0;
-                        int b0 = 0;
-
-                        int RED = r0;
-                        int GREEN = g0;
-                        int BLUE = b0;
-                        int N = 50;
-
-                        start.setR(radius);
-                        for (Dot punto : puntos) {
-                            System.out.println((int) punto.toPixelsX(10) - 5);
-                            g.fillOval((int) punto.toPixelsX(10) - 2, (int) punto.toPixelsY(10) - 2, 5, 5);
-                            if (start.isContain(punto)) {
-                                g.setColor(Color.GREEN);
-                                g.fillOval((int) punto.toPixelsX(10) - 2, (int) punto.toPixelsY(10) - 2, 5, 5);
-                                for (int i = 0; i < 50; i++) {
-                                    RED += (255 - r0) / N;
-                                    GREEN += (255 - g0) / N;
-                                    BLUE += (255 - b0) / N;
-                                    drawArea(g, new Color(RED, GREEN, BLUE));
-                                    repaint();
-                                    Thread.sleep(1000 / 60);
-                                }
-
-                                for (int i = 0; i < 50; i++) {
-                                    RED -= (255 - r0) / N;
-                                    GREEN -= (255 - g0) / N;
-                                    BLUE -= (255 - b0) / N;
-                                    drawArea(g, new Color(RED, GREEN, BLUE));
-                                    repaint();
-                                    Thread.sleep(1000 / 60);
-                                }
-
-                            } else {
-                                g.setColor(Color.RED);
-                                g.fillOval((int) punto.toPixelsX(10) - 2, (int) punto.toPixelsY(10) - 2, 5, 5);
-                            }
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }).start();
-        }
-
-        private class DrawMouseListener extends MouseAdapter {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                Graphics g;
-                //start.setR(radius);
-                g = ((DrawPanel) e.getSource()).getGraphics();
-                Dot dots = new Dot(toDecartX(e.getX(), 10), toDecartY(e.getY(), 10));
-                System.out.println("Mouse Clicked: (" + e.getX() + ", " + e.getY() + ")");
+    private class DrawMouseListener extends MouseAdapter {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            Graphics g;
+            startForm.setR(R);
+            g = ((DrawPanel) e.getSource()).getGraphics();
+            Dot dots = new Dot(toDecartX(e.getX(), R), toDecartY(e.getY(), R));
+            if(!puntos.contains(dots)) {
                 puntos.add(dots);
-                ((DrawPanel) e.getSource()).startAnimation(g, 10, puntos);
-                System.out.println(puntos);
+                showDots(g, puntos, startForm, R);
                 nowDot.setText(dots.toString());
             }
         }
