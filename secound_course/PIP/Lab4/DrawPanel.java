0b8205abfb0fcb9e1a0a7c1a9f15e93d480bfff5
Dmitry Parshikov <dmitry.parshikov28@gmail.com>
2016-11-23 23:20:49 +0300

    Update DrawPanel.java

--- secound_course/PIP/Lab4/DrawPanel.java
+++ secound_course/PIP/Lab4/DrawPanel.java
@@ -1,34 +1,28 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DrawPanel extends JPanel {
 
-    public static final Color COLOR_BLACK = new Color(0,0,0);
-    public static final Color LIGHT_GREEN = new Color(129, 192, 187);
-    public static final Color COLOR_BROWN = new Color(150,75,0);
-    public static Color areaColor = COLOR_BLACK;
+    public  final Color COLOR_BLACK = new Color(0, 0, 0);
+    public  final Color LIGHT_GREEN = new Color(129, 192, 187);
+    public  Color areaColor = COLOR_BLACK;
 
-
-    public static final int HEIGHT = 9;
-    public static final int WEIGHT = 9;
     public static int CENTER_X;
     public static int CENTER_Y;
-    public static final int START_RADIUS = 100 ;
+    public static final int START_RADIUS = 100;
     public static final int POINT_RADIUS = 5;
     public static final int MARGIN = 9;
-    public JTextArea a = new JTextArea();
-    private List<Dot> puntos = new ArrayList<Dot>();
-    DotAnimation dotAnimation;
-    Form start = new Form(10);
+    public List<Dot> dots = new ArrayList<>();
+    public Form form = new Form(10);
+    int r;
 
     public DrawPanel() {
         setBackground(LIGHT_GREEN);
         setSize(462, 462);
-        addMouseListener(new DrawMouseListener());
     }
 
     public void paintComponent(Graphics g) {
@@ -42,8 +36,8 @@ public class DrawPanel extends JPanel {
 
         drawCoordinate(g, h, w);
         drawHatching(g, h, w);
-        drawArea(g, Color.BLUE);
-
+        drawArea(g, areaColor);
+        showDots(g, dots, form, r);
     }
 
     public void drawArea(Graphics g, Color color) {
@@ -80,20 +74,21 @@ public class DrawPanel extends JPanel {
         g.drawString("-R / 2", w / 2 - 30, h / 2 + START_RADIUS / 2);
 
         drawArrow(g, h, w);
-        System.out.println(puntos);
     }
 
     private void drawRectangle(Graphics g, Color color) {
-        g.fillRect(CENTER_X , CENTER_Y, START_RADIUS, START_RADIUS);
+        g.drawLine(CENTER_X, CENTER_Y, CENTER_X + START_RADIUS, CENTER_Y);
+        g.fillRect(CENTER_X, CENTER_Y, START_RADIUS, START_RADIUS);
     }
 
     private void drawArc(Graphics g, Color color) {
-        g.fillArc(CENTER_X - START_RADIUS / 2 , CENTER_Y - START_RADIUS / 2 , START_RADIUS, START_RADIUS, 0, 90);
+        g.drawArc(CENTER_X - START_RADIUS / 2, CENTER_Y - START_RADIUS / 2, START_RADIUS, START_RADIUS, 0, 90);
+        g.fillArc(CENTER_X - START_RADIUS / 2, CENTER_Y - START_RADIUS / 2, START_RADIUS, START_RADIUS, 0, 90);
     }
 
     private void drawTriangle(Graphics g, Color color) {
         int[] xCoordinate = {CENTER_X, CENTER_X - START_RADIUS, CENTER_X};
-        int[] yCoordinate = {CENTER_Y - START_RADIUS, CENTER_Y , CENTER_Y};
+        int[] yCoordinate = {CENTER_Y - START_RADIUS, CENTER_Y, CENTER_Y};
         g.fillPolygon(xCoordinate, yCoordinate, 3);
     }
 
@@ -109,75 +104,54 @@ public class DrawPanel extends JPanel {
         g.drawLine(w / 2 - 3, h / 2 + START_RADIUS / 2, w / 2 + 3, h / 2 + START_RADIUS / 2);
     }
 
-    public double toDecartX(double x, double R) {
-        return (x - DrawPanel.CENTER_X) * R / DrawPanel.START_RADIUS;
-    }
-
-    public double toDecartY(double y, double R) { return (-y + DrawPanel.CENTER_Y) * R / DrawPanel.START_RADIUS; }
-
-    public  void startAnimation(Graphics g, int radius, List<Dot> puntos) {
-        new Thread(new Runnable() {
-            public void run() {
-                try {
-                    int r0 = 1;
-                    int g0 = 0;
-                    int b0 = 0;
-
-                    int RED = r0;
-                    int GREEN = g0;
-                    int BLUE = b0;
-                    int N = 50;
-
-                    start.setR(radius);
-                    for (Dot punto : puntos) {
-                        System.out.println((int) punto.toPixelsX(10) - 5);
-                        g.fillOval((int) punto.toPixelsX(10) - 2, (int) punto.toPixelsY(10) - 2, 5, 5);
-                        if (start.isContain(punto)) {
-                            g.setColor(Color.GREEN);
-                            g.fillOval((int) punto.toPixelsX(10) - 2, (int) punto.toPixelsY(10) - 2, 5, 5);
-                            for (int i = 0; i < 50; i++) {
-                                RED += (255 - r0) / N;
-                                GREEN += (255 - g0) / N;
-                                BLUE += (255 - b0) / N;
-                                drawArea(g, new Color(RED, GREEN, BLUE));
-                                repaint();
-                                Thread.sleep(1000 / 60);
-                            }
-
-                            for (int i = 0; i < 50; i++) {
-                                RED -= (255 - r0) / N;
-                                GREEN -= (255 - g0) / N;
-                                BLUE -= (255 - b0) / N;
-                                drawArea(g, new Color(RED, GREEN, BLUE));
-                                repaint();
-                                Thread.sleep(1000 / 60);
-                            }
-
-                        } else {
-                            g.setColor(Color.RED);
-                            g.fillOval((int) punto.toPixelsX(10) - 2, (int) punto.toPixelsY(10) - 2, 5, 5);
-                        }
-                    }
-                }catch(InterruptedException e) {
-                    e.printStackTrace();
-                }
+    public void showDots(Graphics g, java.util.List<Dot> dots, Form form, int R) {
+        for(Dot dot : dots) {
+            if(form.isContain(dot)) {
+                g.setColor(Color.GREEN);
+                g.fillOval((int) dot.toPixelsX(R) - 2, (int) dot.toPixelsY(R) - 2, DrawPanel.POINT_RADIUS, DrawPanel.POINT_RADIUS);
+            } else {
+                g.setColor(Color.RED);
+                g.fillOval((int) dot.toPixelsX(R) - 2, (int) dot.toPixelsY(R) - 2, DrawPanel.POINT_RADIUS, DrawPanel.POINT_RADIUS);
             }
-        }).start();
-    }
-
-    private class DrawMouseListener extends MouseAdapter {
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            Graphics g;
-            //start.setR(radius);
-            g = ((DrawPanel)e.getSource()).getGraphics();
-            Dot dots = new Dot(toDecartX(e.getX(), 10), toDecartY(e.getY(), 10));
-            System.out.println("Mouse Clicked: (" + e.getX() + ", " + e.getY() + ")");
-            puntos.add(dots);
-            ((DrawPanel) e.getSource()).startAnimation(g, 10, puntos);
-            System.out.println(puntos);
-
         }
     }
-}
 
+    public void startAnimation(Graphics g) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    int main_red = COLOR_BLACK.getRed();
+                    int main_green = COLOR_BLACK.getGreen();
+                    int main_blue = COLOR_BLACK.getBlue();
+
+                    int RED = main_red;
+                    int GREEN = main_green;
+                    int BLUE = main_blue;
+                    int N = 50;
+
+                    for (int i = 0; i < N; i++) {
+                        RED += (255 - main_red) / N;
+                        GREEN += (255 - main_green) / N;
+                        areaColor = new Color(RED, GREEN, BLUE);
+                        repaint();
+                        // drawArea(g, new Color(RED, GREEN, BLUE));
+                        Thread.sleep(1000 / 50);
+                        //drawPanel.repaint();
+                    }
+                    for (int i = 0; i < N; i++) {
+                        RED -= (255 - main_red) / N;
+                        GREEN -= (255 - main_green) / N;
+                        areaColor = new Color(RED, GREEN, BLUE);
+                        repaint();
+//                        drawArea(g, new Color(RED, GREEN, BLUE));
+                        Thread.sleep(1000 / 50);
+                        // drawPanel.repaint();
+                    }
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(DotAnimation.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+    }
+}
