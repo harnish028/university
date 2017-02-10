58393d49123bacd8871380781ba492a21a4bc75d
Dmitry Parshikov <dmitry.parshikov28@gmail.com>
2017-02-10 23:34:30 +0300

    Create Dot.java

---
+++ secound_course/PIP/Lab5/Dot.java
@@ -0,0 +1,36 @@
+public class Dot {
+
+    private double x;
+    private double y;
+
+    public Dot(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public double getY() { return y; }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double toPixelsX(double R) { return (x * DrawPanel.START_RADIUS) / R + DrawPanel.CENTER_X;}
+
+    public double toPixelsY(double R) { return (-y * DrawPanel.START_RADIUS) / R + DrawPanel.CENTER_Y;}
+
+    @Override
+    public String toString() {
+        return "com.pip.lab5client.Dot{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
